596b1ed617d8af14ed4116ce927071b68dd00af3
the-electro-punk <bwr2555@hotmail.com>
2021-11-04 17:19:43 -0500

    created inquirer code and database

--- assets/node-input.js
+++ assets/node-input.js
@@ -1,3 +1,63 @@
 const fs = require('fs')
 const inquirer = require('inquirer');
 
+inquirer.prompt({
+    questions: [
+        {
+        // asks for filename; didn't know how to use ONLY at start so must input for every employee entered
+        type: 'input', 
+        name:"input_name", 
+        message:'what is filename.html?',
+        validate: (answer) => {
+            if(answer === '') {
+                return 'please enter a valid name'
+            }
+                return true
+            }
+        },
+        {
+        type:'input',
+        name: 'department_question',
+        message: 'which department?',
+        validate: (answer) => {
+            if(answer === '') {
+                return 'please enter a valid name'
+            }
+            return true
+            }
+        },
+        {
+        type:'input',
+        name: 'employee_question',
+        message: 'employee name?',
+        validate: (answer) => {
+            if(answer === '') {
+                return 'please enter a valid name'
+            }
+            return true
+            }
+        },
+        {
+        type:'input',
+        name: 'role_question',
+        message: 'what is employee\'s position?',
+        validate: (answer) => {
+            if(answer === '') {
+                return 'please enter a valid name'
+            }
+            return true
+            }
+        },
+    ]
+}).then(answers => { 
+    const fileName = answers.items[i].input_name;
+    const department = answers.items[i].department_question
+    const nameEmp = answers.items[i].employee_question;
+    const position = answers.items[i].role_question;
+
+    let fullstring = fileName + department + nameEmp + position
+    fs.appendFile(fileName, fullstring, function (err) {
+        if (err) throw err;
+        console.log('saved')
+    })
+})
--- employees.js
+++ employees.js
@@ -1,20 +1,39 @@
-// sets up the database basics
-const {Model, Datatypes} = require('sequelize')
-const sequelize = require('../assets/connection.js')
+// // sets up the database basics
+// const {Model, Datatypes} = require('sequelize')
+// const sequelize = require('../assets/connection.js')
 
-// this is the database
-class Employees extends Model {}
+// // this is the database
+// class Employees extends Model {}
 
-// this creates the content of the database
-Employees.init(
-    {
-        department: Datatypes.STRING,
-        employee: Datatypes.STRING,
-        position: Datatypes.STRING
-    },
-    {
-        sequelize
-    }
-);
+// // this creates the content of the database
+// Employees.init(
+//     {
+//         department: Datatypes.STRING,
+//         employee: Datatypes.STRING,
+//         position: Datatypes.STRING
+//     },
+//     {
+//         sequelize
+//     }
+// );
 
-module.exports = Employees;
+// module.exports = Employees;
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "bwr2555@hotmail.com",
+  password: "Majora-2000",
+  database: "GizmonicDB"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "CREATE TABLE employees (department VARCHAR(60), employee VARCHAR(60), position VARCHAR(100))";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  });
+});
