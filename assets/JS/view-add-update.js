b9c3082b0da281ef0b8846cc52b2bc01d6bcfae6
the-electro-punk <88555154+the-electro-punk@users.noreply.github.com>
2022-04-14 12:58:07 -0500

    added list options in inquirer

--- assets/JS/view-add-update.js
+++ assets/JS/view-add-update.js
@@ -1,9 +1,10 @@
 const fs = require('fs')
 const inquirer = require('inquirer');
+// const PathPrompt = require('inquirer-path')
 
 const express = require('express')
 const router = express.Router()
-const db = require('../database')
+// const db = require('../database')
 
 const mysql = require('mysql')
 
@@ -15,25 +16,36 @@ var connection = mysql.createConnection({
     user: 'root',
     password: 'password',
     database: 'Acme',
-    port: '3456',
+    port: '3306',
 })
 
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("connected");
-    con.query("Create Acme Database",
-    function (err, result) {
-        if (err) throw err;
-        console.log("database created")
-    })
-})
+// con.connect(function(err) {
+//     if (err) throw err;
+//     console.log("connected");
+//     con.query("Create Acme Database",
+//     function (err, result) {
+//         if (err) throw err;
+//         console.log("database created")
+//     })
+// })
 
-router.get('/user-list',
-function(req,res,next) {
-    var sql="SELECT * FROM users";
-    db.query(sql, function (err, data, fields) {if (err) throw err;
-    res.render('user-list', {
-        title: 'User List', userdata: data
-        })
-    })
-})
+inquirer.prompt([
+    {
+        type: 'list',
+        name: 'options',
+        message: 'what would you like to do?',
+        choices: ['view all departments', 'view all roles', 'view all employees', 'add department', 'add role', 'add employee']
+    }
+]).then(answers => {
+    console.info('Answer:', answers.options);
+});
+
+// router.get('/user-list',
+// function(req,res,next) {
+//     var sql="SELECT * FROM users";
+//     db.query(sql, function (err, data, fields) {if (err) throw err;
+//     res.render('user-list', {
+//         title: 'User List', userdata: data
+//         })
+//     })
+// })
